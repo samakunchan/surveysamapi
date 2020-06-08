485ba2d08de1f35fe2312a6c92185d6b69fdaec8
Samakunchan <samakunchan@gmail.com>
2020-06-09 01:20:07 +0200

    feat(list): survey
    - Method list added and configured
    - List method use group survey_list to prevent circular dependency

--- src/Controller/SurveyController.php
+++ src/Controller/SurveyController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -21,10 +22,7 @@ class SurveyController extends AbstractController
      */
     public function list(SurveyRepository $surveyRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/SurveyController.php',
-        ]);
+        return $this->json($surveyRepository->findAll(), Response::HTTP_OK, [], ['groups' => ['survey_list']]);
     }
 
     /**
