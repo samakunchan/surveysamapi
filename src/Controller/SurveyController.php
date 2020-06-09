4c30a7fb7825b7a9925102b800492ff43a0790ef
Samakunchan <samakunchan@gmail.com>
2020-06-09 11:48:19 +0200

    feat(create): survey
    - Method create added and configured
    - try/catch added for handling syntax error

--- src/Controller/SurveyController.php
+++ src/Controller/SurveyController.php
@@ -4,12 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Survey;
 use App\Repository\SurveyRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -30,7 +33,7 @@ class SurveyController extends AbstractController
      * @param Survey $survey
      * @return JsonResponse
      */
-    public function show(Survey $survey)
+    public function show(Survey $survey): JsonResponse
     {
         return $this->json($survey, Response::HTTP_OK, [], ['groups' => ['survey_show']]);
     }
@@ -45,10 +48,31 @@ class SurveyController extends AbstractController
      */
     public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, ValidatorInterface $validator)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/SurveyController.php',
-        ]);
+        try {
+            $datas = $request->getContent();
+            $survey = $serializer->deserialize($datas, Survey::class, 'json');
+            $errors = $validator->validate($survey);
+            if (count($errors) > 0) {
+                return $this->json($errors, Response::HTTP_BAD_REQUEST);
+            }
+            $survey->setCreatedAt(new DateTime());
+            $entityManager->persist($survey);
+            $entityManager->flush();
+            return $this->json(
+                $survey,
+                Response::HTTP_CREATED,
+                ['location' => $this->generateUrl('survey_show', ['id' => $survey->getId()], UrlGeneratorInterface::ABSOLUTE_URL)],
+                ['groups' => ['survey_show']]
+            );
+        } catch (NotEncodableValueException $e) {
+            return $this->json(
+                [
+                    'status' => Response::HTTP_BAD_REQUEST,
+                    'message' => $e->getMessage()
+                ],
+                Response::HTTP_BAD_REQUEST
+            );
+        }
     }
 
     /**
